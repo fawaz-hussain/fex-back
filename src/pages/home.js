76d0348d2f309c37b3e796e47638b4b2781a7b52
ktfawazhussain <ktfawazhussain@gmail.com>
2024-04-09 14:34:41 +0530

    created intro page

---
+++ src/pages/home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import fexback from '../images/fex-back.png'
+import '../App.css'
+
+const Home = () => {
+  return (
+    <div className='main'>
+        <div className="face">
+            <div className="eyes">
+                <div className="eye"></div>
+                <div className="eye"></div>
+            </div>
+        </div>
+        <img src={fexback} alt='' className='main-page-logo' />
+        <div className='pad'></div>
+        <div className='main-btn'>
+            <button id="btn">Register</button>
+            <div className='btn-space'></div>
+            <button id="btn">Login</button>
+        </div>
+    </div>
+  )
+}
+
+export default Home
